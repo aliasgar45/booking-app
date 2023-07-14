da3a8ec743ccaf78b12697e3a4b8151d16ee7f6a
Aliasgar <aliasgardf@gmail.com>
2023-07-15 00:58:41 +0530

    Reservation Functionality Added

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -4,12 +4,14 @@ import ClientOnly from '@/app/components/ClientOnly';
 import EmptyState from '@/app/components/EmptyState';
 import React from 'react'
 import ListingClient from './ListingClient';
+import getReservations from '@/app/actions/getReservations';
 
 interface IParams {
     listingId?: string;
 }
 const ListingPage = async ({params} : {params: IParams}) => {
     const listing = await getListingById(params);
+    const reservations = await getReservations(params);
     const currentUser = await getCurrentUser();
 
     if(!listing) {
@@ -24,6 +26,7 @@ const ListingPage = async ({params} : {params: IParams}) => {
     <ClientOnly>
         <ListingClient
         listing = {listing}
+        reservations={reservations}
         currentUser = {currentUser}
         />
     </ClientOnly>
