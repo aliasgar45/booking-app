0a072c0d1358088c923d6b07d18de99636e8be5f
Aliasgar <aliasgardf@gmail.com>
2023-07-22 19:19:12 +0530

    Added favourites page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -105,7 +105,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                         label='My Trips'
                         />
                         <MenuItem
-                        onClick={()=>{}}
+                        onClick={()=> router.push('/favourites')}
                         label='My favourites'
                         />
                         <MenuItem
